4f35ac7bddb9a27cd05fbe3d9485f1dfdbb644e2
AthThobari <thobari.mutu@gmail.com>
2025-08-28 09:57:32 +0700

    feat(project): init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/header/Header";
+
+export function App() {
+  return (
+    <div className="app">
+     <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,69 @@
+import React, { useEffect, useState } from "react";
+import "./header.css";
+const Header = () => {
+    const [active, setActive] = useState(window.location.hash || "#home")
+
+    useEffect(() => {
+      const savedActive = localStorage.getItem("activeMenu");
+      if (savedActive) {
+        setActive(savedActive)
+      window.location.hash = savedActive
+      }
+    }, [])
+
+    useEffect(() => {
+      localStorage.setItem("activeMenu", active);
+      window.location.hash = active
+    }, [active])
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav__logo">
+          Web Personal Profile
+        </a>
+
+        <div className="nav__menu">
+          <ul className={`nav__list ${true ? "":"grid"}`}>
+            <li className="nav__item">
+              <a href="#home" onClick={() => setActive("#home")} className={`nav__link ${active === "#home" ? "active-link" : ""}`}>
+                <i className="uil uil-estate nav__icon"></i> Home
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#about" onClick={() => setActive("#about")} className={`nav__link ${active === "#about" ? "active-link" : ""}`}>
+                <i className="uil uil-user nav__icon"></i> About
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#skills" onClick={() => setActive("#skills")} className={`nav__link ${active === "#skills" ? "active-link" : ""}`}>
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#services" onClick={() => setActive("#services")} className={`nav__link ${active === "#services" ? "active-link" : ""}`}>
+                <i className="uil uil-briefcase-alt nav__icon"></i> Services
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#portfolio" onClick={() => setActive("#portfolio")} className={`nav__link ${active === "#portfolio" ? "active-link" : ""}`}>
+                <i className="uil uil-scenery nav__icon"></i> Portfolio
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" onClick={() => setActive("#contact")} className={`nav__link ${active === "#contact" ? "active-link" : ""}`}>
+                <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+          <i className="uil uil-times nav__close"></i>
+        </div>
+
+        <div className="nav__toggle">
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
